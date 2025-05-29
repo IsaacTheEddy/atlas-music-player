d2e63e2247aa63ea99ae4c9f7187dcd85eb2f5b1
Your Name <isaactheedwards@gmail.com>
2025-05-28 22:25:04 -0500

    Added a top gap to make it look alittle better

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -134,7 +134,7 @@ export const App: React.FC = () => {
         }}
       >
         <div className="flex h-full min-h-screen flex-col bg-KindaGray dark:bg-DarkChocalate dark:text-white">
-          <div className="mx-auto flex flex-col rounded-2xl border-8 border-double border-Mocha md:flex-row">
+          <div className="mx-auto mt-6 flex flex-col rounded-2xl border-8 border-double border-Mocha md:flex-row">
             <div className="min-h-80% flex h-fit flex-col justify-between rounded-es-lg border-Lightpink p-4 md:border-r-4 dark:border-Mocha">
               <MusicPlayer />
             </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,5 @@ import { useCurrentSongContext } from "./context";
 export default function Footer() {
   const year = new Date().getFullYear();
   const { isPlaying, shuffle, loading } = useCurrentSongContext();
-  return (
-    <div className="p-8 text-center">
-      &copy; {year} Atlas School
-      <div>
-        {`${isPlaying.toString()} + ${shuffle.toString()} + ${loading.toString()}`}
-      </div>
-    </div>
-  );
+  return <div className="p-8 text-center">&copy; {year} Atlas School</div>;
 }
